659d996f80ec889fc55cd8a3a3d7f757f6763d19
Cesar Murillo <namiayumis@gmail.com>
2020-04-26 22:48:17 +0900

    get mongo database

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
   dev: process.env.NODE_ENV !== 'production',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3005,
   cors: process.env.CORS,
   dbUser: process.env.DB_USER,
   dbPassword: process.env.DB_PASSWORD,
