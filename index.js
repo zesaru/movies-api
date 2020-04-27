738634fbea83cec4c9e7f2642aa223338b673143
Cesar Murillo <namiayumis@gmail.com>
2020-04-27 18:49:56 +0900

    ¿Qué es un middleware? Capa de manejo de errores usando un middleware

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const app = express();
 
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js');
+const {
+  logErrors,
+  errorHandler
+} = require('./utils/middleware/errorHandlers.js');
 
 app.use(express.json());
 
 moviesApi(app);
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function() {
   console.log(`Listening http://localhost:${config.port}`);
